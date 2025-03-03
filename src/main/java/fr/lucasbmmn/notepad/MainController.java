41536cebbde31cf3357d5848462647b783bfe077
Lucas <lumaga.bm@gmail.com>
2025-03-03 22:42:22 +0100

    Controller skeleton

--- src/main/java/fr/lucasbmmn/notepad/MainController.java
+++ src/main/java/fr/lucasbmmn/notepad/MainController.java
@@ -2,6 +2,75 @@ package fr.lucasbmmn.notepad;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class MainController {
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Label posLabel;
+    @FXML
+    private Label characterLabel;
+    @FXML
+    private Label encodingLabel;
+
+    @FXML
+    private void actionNew() {
+
+    }
+
+    @FXML
+    private void actionOpen() {
+
+    }
+
+    @FXML
+    private void actionSave() {
+
+    }
+
+    @FXML
+    private void actionSaveAs() {
+
+    }
+
+    @FXML
+    private void actionSettings() {
+
+    }
+
+    @FXML
+    private void actionQuit() {
+
+    }
+
+    @FXML
+    private void actionUndo() {
+
+    }
+
+    @FXML
+    private void actionRedo() {
+
+    }
+
+    @FXML
+    private void actionCut() {
+
+    }
+
+    @FXML
+    private void actionCopy() {
+
+    }
+
+    @FXML
+    private void actionPaste() {
+
+    }
+
+    @FXML
+    private void actionDelete() {
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fr.lucasbmmn.notepad {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens fr.lucasbmmn.notepad to javafx.fxml;
